e53a3bca84733067f7f624cad97cbfcfd152943b
colinries <colin.blaine.ries@gmail.com>
2018-01-16 04:47:41 -0800

    Enqueuing script modified

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
-function my_theme_enqueue_styles() {
+
+function child_styles() {
+  wp_enqueue_style( 'parent_style', get_template_directory_uri() . '/style.css')
+}
+
+add_action( 'wp_enqueue_scripts', 'child_styles');
+/*function my_theme_enqueue_styles() {
 
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
@@ -10,5 +16,5 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );*/
 ?>
